5a4a4cfa6a0cb0bd7c0ff6b89272dabc387aba48
Whitham D. Reeve II <thetawaves@gmail.com>
2015-10-13 10:53:03 -0800

    Basic code changes to support a round and a rectangular bobbin.

--- svgplay.go
+++ svgplay.go
@@ -6,8 +6,9 @@ package main
 
 import (
 	"flag"
-	//"fmt"
+	"fmt"
 	"github.com/ajstarks/svgo"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -15,6 +16,7 @@ import (
 
 var (
 	httpListen = flag.String("port", "9004", "port to listen on")
+	dynamic    = flag.Bool("dynamic", false, "Dynamically reload the front page")
 )
 
 var (
@@ -46,16 +48,80 @@ func main() {
 // its contents will be put in the interface's text area.
 // Otherwise, the default "hello, world" program is displayed.
 func FrontPage(w http.ResponseWriter, req *http.Request) {
-	w.Write(frontPage)
+	if *dynamic {
+		page, err := ioutil.ReadFile("client/frontPage.html")
+		if err != nil {
+			log.Println(err)
+		}
+		w.Write(page)
+	} else {
+
+		w.Write(frontPage)
+	}
+}
+
+func formString(req *http.Request, name string) (string, error) {
+	var s string
+	var err error
+	if len(req.Form[name]) > 0 {
+		s = req.Form[name][0]
+	} else {
+		err = fmt.Errorf("Form field %s not provided.", name)
+	}
+	return s, err
 }
 
 // Compile is an HTTP handler that reads Go source code from the request,
 // runs the program (returning any errors),
 // and sends the program's output as the HTTP response.
 func GenSVG(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "image/svg+xml")
+	err := req.ParseForm()
+	if err != nil {
+		log.Println(err)
+	}
+	style, err := formString(req, "s")
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	if style == "rect" {
+		diameter, err := formInt(req, "bd")
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+
+		w.Header().Set("Content-Type", "image/svg+xml")
+		genrect(w, diameter)
+	} else {
+		w.Header().Set("Content-Type", "image/svg+xml")
+		gencir(w)
+	}
+}
+
+var (
+	canvas_width  = 500
+	canvas_height = 500
+	middle_width  = canvas_width / 2
+)
+
+func genrect(w io.Writer) {
 	s := svg.New(w)
-	s.Start(500, 500)
-	s.Circle(250, 250, 125, "fill:none;stroke:black")
+	s.Start(canvas_width, canvas_height)
+
+	s.Rect(middle_width-125, canvas_height/4-75, 100, 100, "fill:none;stroke:black")
+	s.Rect(middle_width-125, 2*canvas_height/3, 250, canvas_height/3, "fill:none;stroke:black")
+
+	s.End()
+}
+
+func gencir(w io.Writer) {
+	s := svg.New(w)
+	s.Start(canvas_width, canvas_height)
+
+	s.Circle(middle_width, canvas_height/3, 125, "fill:none;stroke:black")
+	s.Rect(middle_width-125, 2*canvas_height/3, 250, canvas_height/3, "fill:none;stroke:black")
+
 	s.End()
 }
